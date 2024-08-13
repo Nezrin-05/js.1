3b12dd869737b189f977279110a4e218c053b174
Nezrin-05 <nezrinnuriyeva70@gmail.com>
2024-08-13 09:10:28 +0400

    homework js

---
+++ app.js
@@ -0,0 +1,36 @@
+// console.log("Nezrin");
+// var age = 25;
+// console.log(age);
+
+// let age1 = 30;
+// console.log(age1)
+
+// const age2 = 35;
+// console.log(age2)
+
+
+// var data = 10;
+// var data = "Nezrin"
+// console.log(data)
+
+
+// const name = "Nezrin"
+// console.log(name)
+
+
+// let a = 40;
+// let b = 28;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a * b);
+// console.log(a / b);
+
+
+//   let num = 95;
+// console.log(Math.round(Math.random(num)));
+
+// console.log(Math.round()(Math.random() + 95) (Math.random() + 96 ) (Math.random() + 97 ) (Math.random() + 98 ) (Math.random() + 99 ) (Math.random() + 100 ))
+// console.log(Math.round((Math.random((Math.random((Math.random((Math.random((Math.random((Math.random() + 100)) + 99)) + 98)) + 97)) + 96)) + 95)))
+
+console.log(Math.floor(Math.random() * 6)+ 95);
